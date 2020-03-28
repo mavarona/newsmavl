daec8f135f200d83a01b3a8acee242d47d18be0d
mavl <mvaronamac@gmail.coml>
2020-03-28 23:08:59 +0100

    Add action sheet actions in a header card

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-news',
@@ -13,12 +14,45 @@ export class NewsComponent implements OnInit {
   @Input() newData: Article;
   @Input() index: Number;
 
-  constructor( private iab: InAppBrowser ) { }
+  constructor( private iab: InAppBrowser,
+              private actionSheetCtrl: ActionSheetController ) { }
 
   ngOnInit() {}
 
   openNews() {
      const browser = this.iab.create(this.newData.url, '_system');
+  }
+
+  async launchMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share',
+          handler: () => {
+            console.log('Shared clicked');
+          }
+        },
+        {
+          text: 'Favoritos',
+          icon: 'star',
+          handler: () => {
+            console.log('Favoritos');
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancel click');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+
   }
 
 }
